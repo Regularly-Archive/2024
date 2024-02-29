177b0583d915acbfe6805246c84c8aeac53f150f
qinyuanpei <qinyuanpei@163.com>
2024-02-29 14:48:42 +0800

    完成基于 LangChain 的本地知识库应用开发

--- KnowledgeBase/streaming_api.py
+++ KnowledgeBase/streaming_api.py
@@ -1,94 +1,25 @@
-from langchain_openai import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
-from langchain.prompts.prompt import PromptTemplate
-from langchain.vectorstores.base import VectorStoreRetriever
-from langchain.memory import ConversationBufferMemory
-from rich.console import Console
-from rich.prompt import Prompt
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-import os, pickle, glob
+from langchain_core.outputs import LLMResult
+from typing import Any, Dict, List
 
-os.environ["OPENAI_API_KEY"] = ""
-OUTPUT_DIR = '.\output'
+STOP_FLAG = "[END]"
+START_FLAG = "[START]"
 
-def load_retriever(filePath):
-    with open(filePath, "rb") as f:
-        vectorstore = pickle.load(f)
-        retriever = VectorStoreRetriever(vectorstore=vectorstore)
-        return retriever
-
-def get_basic_qa_chain(baseUrl='', apiKey='', storeFilePath=''):
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo", 
-        temperature=0, 
-        openai_api_base=baseUrl, 
-        openai_api_key=apiKey,
-        streaming=True,
-        callbacks=[StreamingStdOutCallbackHandler()]
-    )
-    retriever = load_retriever(storeFilePath)
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", 
-        return_messages=True, 
-        input_key="question", 
-        output_key="source_documents"
-    )
-    chain = ConversationalRetrievalChain.from_llm(
-        llm=llm, 
-        retriever=retriever, 
-        memory=memory, 
-        return_source_documents=True, 
-        return_generated_question=False
-    )
-    return chain
-
-def get_basic_qa_chain_with_prompt(baseUrl='', apiKey='', storeFilePath=''):
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo", 
-        temperature=0, 
-        openai_api_base=baseUrl, 
-        openai_api_key=apiKey
-    )
-    retriever = load_retriever(storeFilePath)
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", 
-        return_messages=True, 
-        input_key="question", 
-        output_key="source_documents"
-    )
-    prompt = PromptTemplate.from_template("Please answer my question based on the docs")
-    chain = ConversationalRetrievalChain.from_llm(
-        llm=llm, 
-        retriever=retriever, 
-        memory=memory, 
-        return_source_documents=True, 
-        return_generated_question=True,
-        prompt=prompt
-    )
-    return chain
-
-def load_store_files(dir):
-    return list(glob.glob(os.path.join(dir, "*.pkl")))
-
-if __name__ == "__main__":
-    console = Console()
-    console.print("[bold red]---------------------------------")
-    choices = load_store_files(OUTPUT_DIR)
-    choice = Prompt.ask("[bold] Chat with your own knowledge! Please select a vectors storage file [bold]", default=choices[0], show_default=True, choices=choices)
-    chain = get_basic_qa_chain("http://localhost:8080/v1/", 'sk-1234567', choice)
-    console.print("[bold red]---------------------------------")
+class ChainStreamHandler(StreamingStdOutCallbackHandler):
+    def __init__(self, queue):
+        self.queue = queue
     
-    while True:
-        default_question = "请根据你掌握的知识，介绍一下《追风筝的人》这本书的主要内容"
-        question = Prompt.ask("[blue]Question[blue]", default=default_question, show_default=False)
-        for chunk in chain.stream(question):
-            print(chunk)
+    def on_llm_start(self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any) -> None:
+        self.queue.clear()
+        self.queue.put(START_FLAG)
+        self.finished = False
+    
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        self.queue.put(token)
 
-        # console.print("[green]Answer: [/green]" + result['answer'] + "\r\n" + "[green]Reference: [/green]")
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        self.queue.put(STOP_FLAG)
 
-        # for i in range(len(result['source_documents'])):
-        #     document = result['source_documents'][i]
-        #     if 'page' in document.metadata.keys():
-        #         console.print(f"{i+1}. {document.metadata['source']} - {document.metadata['page']} \r\n {document.page_content.strip()}\r\n")
-        #     else:
-        #         console.print(f"{i+1}. {document.metadata['source']} \r\n {document.page_content.strip()}\r\n")
+    def on_llm_error(self, error: Exception, **kwargs):
+        self.queue.put("%s: %s" % (type(error).__name__, str(error)))
+        self.queue.put(STOP_FLAG)
