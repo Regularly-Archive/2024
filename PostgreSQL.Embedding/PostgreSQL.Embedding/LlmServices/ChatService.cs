f69fdbb3629f2b42090ed1cd16df756b1b40ec63
qinyuanpei <qinyuanpei@163.com>
2024-03-08 18:12:59 +0800

    更新项目

--- PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices/ChatService.cs
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices/ChatService.cs
@@ -26,155 +26,20 @@ namespace PostgreSQL.Embedding.LlmServices
             // Todo: 从 sk 中解析应用信息
 
             var app = new LlmApp();
-            var kernal = CreateKernel(app);
+            var kernel = CreateKernel(app);
 
+            var input = await SummarizeHistories(model, kernel);
             switch (app.AppType)
             {
                 case (int)LlmAppType.Chat:
-                    await HandleChat(model, HttpContext, kernal);
+                    var genericChatService = new GenericChatService(kernel, app);
+                    await genericChatService.HandleChat(model, HttpContext, input);
                     break;
                 case (int)LlmAppType.Knowledge:
-                    await HandleKnowledge()
+                    var knowledgeBasedChatService = new KnowledgeBaseChatService(kernel, app, _serviceProvider);
+                    await knowledgeBasedChatService.HandleKnowledge(HttpContext, input);
                     break;
             }
-        }
-
-        private async Task HandleChat(OpenAIModel model, HttpContext HttpContext, Kernel kernel)
-        {
-            if (model.stream)
-            {
-                var result = new OpenAIStreamResult();
-                result.created = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                result.choices = new List<StreamChoicesModel>() { new StreamChoicesModel() { delta = new OpenAIMessage() { role = "assistant" } } };
-                await HandleStramingChat(HttpContext, result, app, msg);
-                HttpContext.Response.ContentType = "application/json";
-                await HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(result1));
-                await HttpContext.Response.CompleteAsync();
-                return;
-            }
-            else
-            {
-                OpenAIResult result2 = new OpenAIResult();
-                result2.created = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                result2.choices = new List<ChoicesModel>() { new ChoicesModel() { message = new OpenAIMessage() { role = "assistant" } } };
-                result2.choices[0].message.content = await SendChat(msg, app);
-                HttpContext.Response.ContentType = "application/json";
-                await HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(result2));
-                await HttpContext.Response.CompleteAsync();
-            }
-        }
-
-        private async Task HandleStramingChat(HttpContext HttpContext, OpenAIStreamResult result, Apps app, string msg)
-        {
-            var _kernel = _kernelService.GetKernel();
-            var temperature = app.Temperature / 100;//存的是0~100需要缩小
-            OpenAIPromptExecutionSettings settings = new() { Temperature = temperature };
-            HttpContext.Response.Headers.Add("Content-Type", "text/event-stream");
-
-            if (string.IsNullOrEmpty(app.Prompt) || !app.Prompt.Contains("{{$input}}"))
-            {
-                //如果模板为空，给默认提示词
-                app.Prompt = app.Prompt.ConvertToString() + "{{$input}}";
-            }
-            //var promptTemplateFactory = new KernelPromptTemplateFactory();
-            //var promptTemplate = promptTemplateFactory.Create(new PromptTemplateConfig(app.Prompt));
-            //var renderedPrompt = await promptTemplate.RenderAsync(_kernel);
-            //Console.WriteLine(renderedPrompt);
-
-            var func = _kernel.CreateFunctionFromPrompt(app.Prompt, settings);
-            var chatResult = _kernel.InvokeStreamingAsync<StreamingChatMessageContent>(function: func, arguments: new KernelArguments() { ["input"] = msg });
-            int i = 0;
-
-            await foreach (var content in chatResult)
-            {
-                result.choices[0].delta.content = content.Content.ConvertToString();
-                string message = $"data: {JsonConvert.SerializeObject(result)}\n\n";
-                await HttpContext.Response.WriteAsync(message, Encoding.UTF8);
-                await HttpContext.Response.Body.FlushAsync();
-                //模拟延迟。
-                await Task.Delay(TimeSpan.FromMilliseconds(50));
-            }
-
-            await HttpContext.Response.WriteAsync("data: [DONE]");
-            await HttpContext.Response.Body.FlushAsync();
-
-            await HttpContext.Response.CompleteAsync();
-        }
-
-        private async Task<string> SendChat(string msg, LlmApp app)
-        {
-            string result = "";
-            if (string.IsNullOrEmpty(app.Prompt) || !app.Prompt.Contains("{{$input}}"))
-            {
-                //如果模板为空，给默认提示词
-                app.Prompt = app.Prompt.ConvertToString() + "{{$input}}";
-            }
-            var _kernel = _kernelService.GetKernel();
-            var temperature = app.Temperature / 100;//存的是0~100需要缩小
-            OpenAIPromptExecutionSettings settings = new() { Temperature = temperature };
-            if (!string.IsNullOrEmpty(app.ApiFunctionList))
-            {
-                _kernelService.ImportFunctionsByApp(app, _kernel);
-                settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions, Temperature = temperature };
-            }
-            var promptTemplateFactory = new KernelPromptTemplateFactory();
-            var promptTemplate = promptTemplateFactory.Create(new PromptTemplateConfig(app.Prompt));
-
-            var func = _kernel.CreateFunctionFromPrompt(app.Prompt, settings);
-            var chatResult = await _kernel.InvokeAsync(function: func, arguments: new KernelArguments() { ["input"] = msg });
-            if (chatResult.IsNotNull())
-            {
-                string answers = chatResult.GetValue<string>();
-                result = answers;
-            }
-            return result;
-        }
-
-        private async Task HandleKnowledge(OpenAIModel model, HttpContext HttpContext, Kernel kernel, string message)
-        {
-            OpenAIResult result3 = new OpenAIResult();
-            result3.created = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-            result3.choices = new List<ChoicesModel>() { new ChoicesModel() { message = new OpenAIMessage() { role = "assistant" } } };
-            result3.choices[0].message.content = await SendKms(msg, app);
-            HttpContext.Response.ContentType = "application/json";
-            await HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(result3));
-            await HttpContext.Response.CompleteAsync();
-        }
-
-        private async Task<string> SendKms(string message, LlmApp app, Kernel kernel)
-        {
-            var _memory = _kMService.GetMemory();
-            string result = "";
-            //知识库问答
-            var filters = new List<MemoryFilter>();
-
-            var kmsidList = app.KmsIdList.Split(",");
-            foreach (var kmsid in kmsidList)
-            {
-                filters.Add(new MemoryFilter().ByTag("kmsid", kmsid));
-            }
-
-            var xlresult = await _memory.SearchAsync(message, index: "kms", filters: filters);
-            string dataMsg = "";
-            if (xlresult != null)
-            {
-                foreach (var item in xlresult.Results)
-                {
-                    foreach (var part in item.Partitions)
-                    {
-                        dataMsg += $"[file:{item.SourceName};Relevance:{(part.Relevance * 100).ToString("F2")}%]:{part.Text}{Environment.NewLine}";
-                    }
-                }
-                KernelFunction jsonFun = _kernel.Plugins.GetFunction("KMSPlugin", "Ask");
-                var chatResult = await _kernel.InvokeAsync(function: jsonFun,
-                    arguments: new KernelArguments() { ["doc"] = dataMsg, ["history"] = "", ["questions"] = message });
-                if (chatResult.IsNotNull())
-                {
-                    string answers = chatResult.GetValue<string>();
-                    result = answers;
-                }
-            }
-            return result;
         }
 
         private Kernel CreateKernel(LlmApp app)
@@ -205,7 +70,7 @@ namespace PostgreSQL.Embedding.LlmServices
             if (model.messages.Count() > 10)
             {
                 //历史会话大于10条，进行总结
-                var msg = await _kernelService.HistorySummarize(_kernel, questions, history.ToString());
+                var msg = await HistorySummarize(kernel, questions, history.ToString());
                 return msg;
             }
             else
