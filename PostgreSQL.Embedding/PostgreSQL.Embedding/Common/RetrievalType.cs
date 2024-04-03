292f1e196f5517df8894108ccfeccb869ab6c2c4
qinyuanpei <qinyuanpei@163.com>
2024-04-03 18:02:36 +0800

    支持全文检索

---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Common/RetrievalType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace PostgreSQL.Embedding.Common
+{
+    public enum RetrievalType
+    {
+        [Description("向量检索")] Vectors = 0,
+        [Description("全文检索")] FullText = 1
+    }
+}
---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices.Abstration/IFullTextSearchService.cs
@@ -0,0 +1,10 @@
+﻿using PostgreSQL.Embedding.Common.Models.KernelMemory;
+
+namespace PostgreSQL.Embedding.LlmServices.Abstration
+{
+    public interface IFullTextSearchService
+    {
+        Task<KMSearchResult> SearchAsync(long knowledgeBaseId, string question, double? minRelevance = 0, int? limit = 5);
+        Task<KMAskResult> AskAsync(long knowledgeBaseId, string question, double? minRelevance = 0);
+    }
+}
