c7a86ab74834c14acee5b2cbd2acbd88c6af520d
qinyuanpei <qinyuanpei@163.com>
2024-04-15 15:16:16 +0800

    增加耗时统计

--- hf-api/embeddings.py
+++ hf-api/embeddings.py
@@ -3,7 +3,11 @@ from models import EmbeddingsRequest, EmbeddingsObjectResponse
 from typing import List
 from utils import Embedding_Model_Cache_Folder as model_cache_folder
 import os
+from utils import timer, createLogger
 
+logger = createLogger(__name__)
+
+@timer(logger=logger)
 def get_embeddings(request: EmbeddingsRequest) -> List[EmbeddingsObjectResponse]: 
     cache_folder = os.path.join(model_cache_folder, request.model)
     model = SentenceTransformer(request.model, cache_folder=cache_folder)
