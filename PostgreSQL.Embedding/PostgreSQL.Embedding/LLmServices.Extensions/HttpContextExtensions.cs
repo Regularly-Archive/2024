6b20ce88ccd0abbea5065f8c993460fb7f9ddee3
qinyuanpei <qinyuanpei@163.com>
2024-04-15 17:36:06 +0800

    Refactor LlmService

--- PostgreSQL.Embedding/PostgreSQL.Embedding/LLmServices.Extensions/HttpContextExtensions.cs
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/LLmServices.Extensions/HttpContextExtensions.cs
@@ -1,11 +1,15 @@
-﻿using PostgreSQL.Embedding.Common;
+﻿using Newtonsoft.Json;
+using PostgreSQL.Embedding.Common;
+using PostgreSQL.Embedding.Common.Models;
+using PostgreSQL.Embedding.LlmServices.LLama;
+using System.Text;
 using System.Web;
 
 namespace PostgreSQL.Embedding.LLmServices.Extensions
 {
     public static class HttpContextExtensions
     {
-        public static string GetOrCreateConversationId(this HttpContext context)
+        public static string GetOrCreateConversationId(this Microsoft.AspNetCore.Http.HttpContext context)
         {
             var conversationId = context.Request.Headers[Constants.HttpRequestHeader_ConversationId].FirstOrDefault();
             if (string.IsNullOrEmpty(conversationId)) conversationId = Guid.NewGuid().ToString();
@@ -13,12 +17,67 @@ namespace PostgreSQL.Embedding.LLmServices.Extensions
             return conversationId;
         }
 
-        public static string GetConversationName(this HttpContext context)
+        public static string GetConversationName(this Microsoft.AspNetCore.Http.HttpContext context)
         {
             var conversationName = context.Request.Headers[Constants.HttpRequestHeader_ConversationName].FirstOrDefault();
             if (string.IsNullOrEmpty(conversationName)) return "未命名会话";
 
             return HttpUtility.UrlDecode(conversationName);
+        }
+
+        public static async Task WriteEmbedding(this Microsoft.AspNetCore.Http.HttpContext context, List<float> embedding)
+        {
+            var result = new OpenAICompatibleEmbeddingResult();
+            result.Data.Add(new OpenAICompatibleEmbeddingDataModel() { Index = 0, Embedding = embedding });
+            if (!context.Response.HasStarted) context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+            await context.Response.CompleteAsync();
+        }
+
+        public static async Task WriteCompletion(this Microsoft.AspNetCore.Http.HttpContext context, string text)
+        {
+            var result = new OpenAICompatibleCompletionResult();
+            result.Choices = new List<OpenAICompatibleCompletionChoiceModel>()
+            {
+                new OpenAICompatibleCompletionChoiceModel() { Index = 0, Text = text }
+            };
+            
+            if (!context.Response.HasStarted) context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+            await context.Response.CompleteAsync();
+        }
+
+        public static async Task WriteChatCompletion(this Microsoft.AspNetCore.Http.HttpContext context, string text)
+        {
+            var result = new OpenAIResult();
+            result.choices = new List<ChoicesModel>()
+            {
+                new ChoicesModel() { index = 0, message = new OpenAIMessage() { role = "assistant", content = text } },
+            };
+
+            if (!context.Response.HasStarted) context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+            await context.Response.CompleteAsync();
+        }
+
+        public static async Task WriteStreamingChatCompletion(this Microsoft.AspNetCore.Http.HttpContext context, IAsyncEnumerable<string> text)
+        {
+            var result = new OpenAIStreamResult();
+            result.choices = new List<StreamChoicesModel>() 
+            {
+                new StreamChoicesModel() { delta = new OpenAIMessage() { role = "assistant" } } 
+            };
+
+            await foreach (var r in text)
+            {
+                result.choices[0].delta.content = r == null ? string.Empty : Convert.ToString(r);
+                string message = $"data: {JsonConvert.SerializeObject(result)}\n\n";
+                await context.Response.WriteAsync(message, Encoding.UTF8);
+                await context.Response.Body.FlushAsync();
+            }
+            await context.Response.WriteAsync("data: [DONE]");
+            await context.Response.Body.FlushAsync();
+            await context.Response.CompleteAsync();
         }
     }
 }
--- PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices/HuggingFace/HuggingFaceService.cs
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices/HuggingFace/HuggingFaceService.cs
@@ -1,39 +1,84 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using PostgreSQL.Embedding.Common.Models;
 using PostgreSQL.Embedding.LlmServices.Abstration;
 using PostgreSQL.Embedding.LlmServices.LLama;
+using System.Net.Http;
+using System.Text;
 
 namespace PostgreSQL.Embedding.LlmServices.HuggingFace
 {
     public class HuggingFaceService : ILlmService
     {
-        private readonly HuggingFaceEmbeddingService _huggingFaceEmbeddingService;
-        public HuggingFaceService(HuggingFaceEmbeddingService embeddingService)
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _baseUrl;
+        public HuggingFaceService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
         {
-            _huggingFaceEmbeddingService = embeddingService;
+            _baseUrl = configuration["HuggingFaceConfig:BaseUrl"];
+            _httpClientFactory = httpClientFactory;
         }
 
-        public Task Chat(OpenAIModel model, HttpContext HttpContext)
+        public async Task<string> ChatAsync(OpenAIModel request)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task ChatStream(OpenAIModel model, HttpContext HttpContext)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task Embedding(OpenAIEmbeddingModel model, HttpContext HttpContext)
-        {
-            var result = new OpenAIEmbeddingResult();
-            result.model = model.model;
-            result.data[0].embedding = await _huggingFaceEmbeddingService.Embedding(model);
-            if (!HttpContext.Response.HasStarted)
+            using (var httpClient = _httpClientFactory.CreateClient())
             {
-                HttpContext.Response.ContentType = "application/json";
+                var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(new Uri($"{_baseUrl}/v1/chat/completions"), httpContent);
+                httpResponse.EnsureSuccessStatusCode();
+
+                var returnContent = await httpResponse.Content.ReadAsStringAsync();
+                var completionResult = JsonConvert.DeserializeObject<OpenAIResult>(returnContent);
+
+                return completionResult.choices[0].message.content;
             }
-            await HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(result));
-            await HttpContext.Response.CompleteAsync();
+        }
+
+        public async IAsyncEnumerable<string> ChatStreamAsync(OpenAIModel request)
+        {
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(new Uri($"{_baseUrl}/v1/chat/completions"), httpContent);
+                httpResponse.EnsureSuccessStatusCode();
+
+                var returnContent = await httpResponse.Content.ReadAsStringAsync();
+                var completionResult = JsonConvert.DeserializeObject<OpenAIResult>(returnContent);
+
+                foreach (var item in completionResult.choices[0].message.content)
+                {
+                    yield return item.ToString();
+                }
+            }
+        }
+
+        public async Task<string> CompletionAsync(OpenAICompletionModel request)
+        {
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(new Uri($"{_baseUrl}/v1/completions"), httpContent);
+                httpResponse.EnsureSuccessStatusCode();
+
+                var returnContent = await httpResponse.Content.ReadAsStringAsync();
+                var completionResult = JsonConvert.DeserializeObject<OpenAICompatibleCompletionResult>(returnContent);
+
+                return completionResult.Choices[0].Text;
+            }
+        }
+
+        public async Task<List<float>> Embedding(OpenAIEmbeddingModel embeddingModel)
+        {
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                var httpContent = new StringContent(JsonConvert.SerializeObject(embeddingModel), Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(new Uri($"{_baseUrl}/v1/embeddings"), httpContent);
+                httpResponse.EnsureSuccessStatusCode();
+
+                var returnContent = await httpResponse.Content.ReadAsStringAsync();
+                var dynamic = JObject.Parse(returnContent);
+
+                return JsonConvert.DeserializeObject<List<float>>(dynamic["data"][0]["embedding"].ToString());
+            }
         }
     }
 }
