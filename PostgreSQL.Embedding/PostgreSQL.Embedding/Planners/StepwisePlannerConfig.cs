8967882452878bb290d661ba71984b3d064ce80d
qinyuanpei <qinyuanpei@163.com>
2024-06-26 15:29:09 +0800

    支持 Planner

---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Planners/StepwisePlannerConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace PostgreSQL.Embedding.Planners
+{
+    public class StepwisePlannerConfig
+    {
+        public string Suffix { get; set; } = @"Let's break down the problem step by step and think about the best approach. Label steps as they are taken. Continue the thought process!";
+
+        public int MaxIterations { get; set; } = 15;
+
+        public int MinIterationTimeMs { get; set; } = 2000;
+
+        public List<string> ExcludedPlugins { get; set; } = new List<string>();
+
+        public List<string> ExcludedFunctions { get; set; } = new List<string>();
+    }
+}
