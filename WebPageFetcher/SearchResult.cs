322cf6bfed6b6448473268bc088b2d95027d4482
qinyuanpei <qinyuanpei@163.com>
2024-07-11 15:57:19 +0800

    增加新示例

---
+++ WebPageFetcher/SearchResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPageFetcher
+{
+    public class SearchResult
+    {
+        public List<Entry> Entries { get; set; } = new List<Entry>();
+    }
+
+    public class Entry
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
