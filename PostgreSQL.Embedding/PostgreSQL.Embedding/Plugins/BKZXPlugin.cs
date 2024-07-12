4efcba0a4486970b436543942ee0bbed41512a03
qinyuanpei <qinyuanpei@163.com>
2024-07-12 15:24:32 +0800

    继续为 Agent 编写插件

--- PostgreSQL.Embedding/PostgreSQL.Embedding/Plugins/BKZXPlugin.cs
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Plugins/BKZXPlugin.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.SemanticKernel;
 using PostgreSQL.Embedding.Common.Attributes;
 using System.ComponentModel;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PostgreSQL.Embedding.Plugins
 {
@@ -15,10 +17,22 @@ namespace PostgreSQL.Embedding.Plugins
 
         [KernelFunction]
         [Description("从中国大百科全书数据库中检索信息")]
-        public async Task<string> Query([Description("关键词/实体")] string entity)
+        public async Task<string> Query([Description("")] string keyword)
         {
+            var pattern = @"\s+";
+            var entities = Regex.Split(keyword, pattern);
+            var stringBuilder = new StringBuilder();
+
             using var httpClient = _httpClientFactory.CreateClient();
-            return await httpClient.GetStringAsync($"https://r.jina.ai/https://h.bkzx.cn/search?query={entity}&sublibId=");
+            foreach (var entity in entities)
+            {
+                var queryResult = await httpClient.GetStringAsync($"https://r.jina.ai/https://h.bkzx.cn/search?query={entity}&sublibId=");
+                stringBuilder.AppendLine(queryResult);
+                stringBuilder.AppendLine();
+                await Task.Delay(3000);
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
