1923426900d9b3dfbe4cafe4f6ccd23374e83107
qinyuanpei <qinyuanpei@163.com>
2024-08-15 09:56:33 +0800

    更新项目

--- PostgreSQL.Embedding/PostgreSQL.Embedding/Controllers/ConversationController.cs
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Controllers/ConversationController.cs
@@ -25,7 +25,7 @@ namespace PostgreSQL.Embedding.Controllers
         }
 
         [HttpPost("{appId}")]
-        public async Task Chat(OpenAIModel model, long appId)
+        public async Task Chat(OpenAIModel model, long appId, CancellationToken cancellationToken = default)
         {
             await _conversationService.Invoke(model, appId, HttpContext);
         }
