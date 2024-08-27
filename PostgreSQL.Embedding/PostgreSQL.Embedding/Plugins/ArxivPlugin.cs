8fe5487db2d9b919dc93197f639d872f0bca1727
qinyuanpei <qinyuanpei@163.com>
2024-08-27 17:54:11 +0800

    支持新插件

---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Plugins/ArxivPlugin.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.SemanticKernel;
+using Newtonsoft.Json;
+using PostgreSQL.Embedding.Common.Attributes;
+using PostgreSQL.Embedding.Common.Models;
+using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace PostgreSQL.Embedding.Plugins
+{
+    [KernelPlugin(Description = "一个支持从 arXiv 上检索学术论文的插件")]
+    public class ArxivPlugin
+    {
+        /// <summary>
+        /// 每次最多检索 5 篇论文
+        /// </summary>
+        private const int MAX_RETRIEVE_NUMBER = 5;
+
+        private readonly XNamespace ATOM_NAMESPACE = "http://www.w3.org/2005/Atom";
+
+        /// <summary>
+        /// 提取英文关键词的提示词
+        /// </summary>
+        private const string EXTRACT_ENGLISH_KEYWORDS_PROMPT =
+            """
+            请整理出用户希望检索的关键词，关键词必须是英文，如果是英文以外的语言，需要转换为英文，多个关键词请使用英文空格分隔。
+            例如：Albert Einstein
+            请不要返回任何与英文关键词无关的内容。
+
+            用户输入：{{$input}}
+            你整理出的英文检索关键词为：
+            """;
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ArxivPlugin(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [KernelFunction]
+        [Description("通过关键字查询学术论文")]
+        public async Task<string> SearchPapersByKeywordsAsync([Description("一个或多个关键词，使用空格隔开")] string keywords, Kernel kernel, int max_results = 5)
+        {
+            var clonedKernel = kernel.Clone();
+
+            var promptTemplate = new CallablePromptTemplate(EXTRACT_ENGLISH_KEYWORDS_PROMPT);
+            promptTemplate.AddVariable("input", keywords);
+            var functionResult = await promptTemplate.InvokeAsync(kernel);
+            var extractedKeywords = functionResult.GetValue<string>();
+
+            // Todo: 支持分页查询
+            var papers = await GetPapersAsync($"https://export.arxiv.org/api/query?search_query=all:{extractedKeywords}");
+            return JsonConvert.SerializeObject(papers);
+        }
+
+        [KernelFunction]
+        [Description("通过 id_list 查询学术论文")]
+        public async Task<string> SearchPapersByIdAsync([Description("一个或多个id，使用英文逗号隔开")] string id_list)
+        {
+            var papers = await GetPapersAsync($"https://export.arxiv.org/api/query?id_list={id_list}");
+            return JsonConvert.SerializeObject(papers);
+        }
+
+        private async Task<IEnumerable<ArxivPaper>> GetPapersAsync(string url)
+        {
+            using var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var feed = await response.Content.ReadAsStringAsync();
+            var doc = XDocument.Parse(feed);
+
+            var papers = new List<ArxivPaper>();
+            foreach (var entry in doc.Root.Elements(ATOM_NAMESPACE + "entry"))
+            {
+                var paper = new ArxivPaper
+                {
+                    Id = entry.Element(ATOM_NAMESPACE + "id").Value.Split('/').LastOrDefault(),
+                    Title = FormatValue(entry.Element(ATOM_NAMESPACE + "title").Value),
+                    Authors = entry.Elements(ATOM_NAMESPACE + "author").Select(a => a.Element(ATOM_NAMESPACE + "name").Value).ToArray(),
+                    Summary = FormatValue(entry.Element(ATOM_NAMESPACE + "summary").Value),
+                    Link = entry.Elements(ATOM_NAMESPACE + "link").FirstOrDefault(l => l.Attribute("type")?.Value == "text/html")?.Attribute("href")?.Value,
+                    PdfLink = entry.Elements(ATOM_NAMESPACE + "link").FirstOrDefault(l => l.Attribute("type")?.Value == "application/pdf")?.Attribute("href")?.Value
+                };
+                papers.Add(paper);
+            }
+
+            return papers;
+        }
+
+        private string FormatValue(string value) => value.Replace("\r", "").Replace("\n", "").Trim();
+    }
+
+    internal class ArxivPaper
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public string Summary { get; set; }
+        public string Link { get; set; }
+        public string PdfLink { get; set; }
+    }
+}
---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Plugins/MailKitPlugin.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.SemanticKernel;
+using MimeKit;
+using PostgreSQL.Embedding.Common.Attributes;
+using System.ComponentModel;
+
+namespace PostgreSQL.Embedding.Plugins
+{
+    [KernelPlugin(Description = "MailKit插件")]
+    public class MailKitPlugin
+    {
+        private const string MAIL_SENDER_NAME = "Wikit";
+        private const string MAIL_SENDER_EMAIL = "qinyuanpei@126.com";
+
+        public MailKitPlugin()
+        {
+
+        }
+
+        [KernelFunction]
+        [Description("使用 MailKit 发送邮件")]
+        public string SendMailAsync([Description("主题")] string subject, [Description("正文")] string body, [Description("收件人")] string receiver)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress(MAIL_SENDER_NAME, MAIL_SENDER_EMAIL));
+            email.To.Add(new MailboxAddress(receiver, receiver));
+
+            email.Subject = subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Plain)
+            {
+                Text = body
+            };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.126.com", 587, true);
+            smtp.Authenticate(MAIL_SENDER_EMAIL, "NIUCTOSAEHORYBDM");
+            return smtp.Send(email);
+        }
+    }
+}
