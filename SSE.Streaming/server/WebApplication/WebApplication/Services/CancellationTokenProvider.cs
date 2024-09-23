77f013dbf1c4d33231e730a9c9b0bd0bf58b4f7e
qinyuanpei <qinyuanpei@163.com>
2024-09-23 09:35:49 +0800

    更新项目

---
+++ SSE.Streaming/server/WebApplication/WebApplication/Services/CancellationTokenProvider.cs
@@ -0,0 +1,23 @@
+﻿
+namespace WebApp.Services
+{
+    public class CancellationTokenProvider : ICancellationTokenProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CancellationTokenProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public CancellationToken GetCancellationToken()
+        {
+            return _httpContextAccessor?.HttpContext?.RequestAborted ?? CancellationToken.None;
+        }
+
+        public Task<CancellationToken> GetCancellationTokenAsync()
+        {
+            var cancellationToken =  _httpContextAccessor?.HttpContext?.RequestAborted ?? CancellationToken.None;
+            return Task.FromResult(cancellationToken);
+        }
+    }
+}
