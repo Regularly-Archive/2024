b4770b7604bbbabe9f745500d60501f44de614a8
qinyuanpei <qinyuanpei@163.com>
2024-09-30 17:39:56 +0800

    调整为异步代码

--- hf-api/api.py
+++ hf-api/api.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from models import CompletionRequest, ChatCompletionRequest, EmbeddingsRequest, EmbeddingsObjectResponse, EmbeddingsResponse, Usage, CompletionResponse, CompletionResponseChoice, ChatCompletionResponse, ChatCompletionResponseChoice, ChatMessage
 from fastapi import FastAPI, HTTPException
-from completion import get_chat_completion, get_text_completion
-from embeddings import get_embeddings
-import os, uvicorn
+from completion import get_chat_completion_async, get_text_completion_async
+from embeddings import get_embeddings_async
+import os, uvicorn, asyncio
 from uvicorn.config import LOGGING_CONFIG
 
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
@@ -11,7 +11,7 @@ app = FastAPI(title='A OpenAI Compatible API for HuggingFace')
 
 @app.post("/v1/embeddings")
 async def text_embeddings(request: EmbeddingsRequest) -> EmbeddingsResponse:
-    embeddings = get_embeddings(request)
+    embeddings = await get_embeddings_async(request)
     if isinstance(request.input, str):
         return EmbeddingsResponse(data=embeddings, model=request.model, usage=Usage(), object="list")
     
@@ -24,7 +24,7 @@ async def text_embeddings(request: EmbeddingsRequest) -> EmbeddingsResponse:
 
 @app.post("/v1/chat/completions")
 async def chat_completions(request: ChatCompletionRequest):
-    text = get_chat_completion(request)
+    text = await get_chat_completion_async(request)
     message = ChatMessage(role='assistant', content=text)
     return ChatCompletionResponse(
         model=request.model, 
@@ -34,7 +34,7 @@ async def chat_completions(request: ChatCompletionRequest):
 
 @app.post("/v1/completions")
 async def completions(request: CompletionRequest): 
-    text = get_text_completion(request)
+    text = await get_text_completion_async(request)
     return CompletionResponse(
         model=request.model, 
         choices=[CompletionResponseChoice(text=text, index=0, finish_reason='length', logprobs=None)],
