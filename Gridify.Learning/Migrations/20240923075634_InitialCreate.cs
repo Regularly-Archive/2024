02ef7ae92d94e4f99b68b2d51a4fd086a1a9f70f
qinyuanpei <qinyuanpei@163.com>
2024-09-30 17:41:01 +0800

    更新示例项目

---
+++ Gridify.Learning/Migrations/20240923075634_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Gridify.Learning.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Sku = table.Column<string>(type: "TEXT", nullable: false),
+                    ModelName = table.Column<string>(type: "TEXT", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false),
+                    Brand = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Brand", "Color", "ModelName", "Sku" },
+                values: new object[,]
+                {
+                    { 1, "Nike1", "Blue", "Soria Tank1", "X000006482043" },
+                    { 2, "Nike2", "Black", "Soria Tank2", "X000006482044" },
+                    { 3, "Nike3", "White", "Soria Tank3", "X000006482045" },
+                    { 4, "Nike4", "Red", "Soria Tank5", "X000006482046" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
