593f18052d01eb4beb86b968d468c70245d3b1d9
qinyuanpei <qinyuanpei@163.com>
2024-09-30 17:41:01 +0800

    更新示例项目

---
+++ Gridify.Learning/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Gridify.Learning.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string ModelName {  get; set; }
+        public string Color {  get; set; }
+        public string Brand { get; set; }
+    }
+}
