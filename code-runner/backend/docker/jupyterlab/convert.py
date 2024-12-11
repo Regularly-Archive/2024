b3e3f5427f4f94241ba76d1f164deee2893970d1
qinyuanpei <qinyuanpei@163.com>
2024-12-11 11:51:45 +0800

    优化 Jupyter

--- code-runner/backend/docker/jupyterlab/convert.py
+++ code-runner/backend/docker/jupyterlab/convert.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import nbformat
-from nbconvert import HTMLExporter
+from nbconvert import HTMLExporter, NotebookExporter
 from nbclient import NotebookClient
 
 def notebook_to_html(notebook_path, output_path, kernel_name=None):
@@ -21,6 +21,23 @@ def notebook_to_html(notebook_path, output_path, kernel_name=None):
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(body)
 
+def notebook_to_notebook(notebook_path, output_path, kernel_name=None):
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        notebook_content = nbformat.read(f, as_version=4)
+    
+    # 执行笔记本
+    client = NotebookClient(notebook_content, kernel_name=kernel_name)
+    client.execute()
+    
+    # 隐藏输入
+    for cell in notebook_content.cells:
+        if cell.cell_type == 'code':
+            cell.metadata['hide_input'] = True
+            cell.source = ''
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        nbformat.write(notebook_content, f)
+
 def main():
     parser = argparse.ArgumentParser(description='Convert Jupyter Notebook to HTML.')
     parser.add_argument('notebook_path', type=str, help='Path to the Jupyter Notebook file')
@@ -30,7 +47,13 @@ def main():
     args = parser.parse_args()
 
     try:
-        notebook_to_html(args.notebook_path, args.output_path, args.kernel)
+        output_format = os.getenv('NBCONVERT_OUTPUT_FORMAT', 'html')
+        if output_format == 'html':
+            notebook_to_html(args.notebook_path, args.output_path, args.kernel)
+        elif output_format == 'notebook':
+            notebook_to_notebook(args.notebook_path, args.output_path, args.kernel)
+        else:
+            raise NotImplementedError(f"unsupported output format '{output_format}' for nbconvert")
 
         absolute_input_path = os.path.abspath(args.notebook_path)
         absolute_output_path = os.path.abspath(args.output_path)
