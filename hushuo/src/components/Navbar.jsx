aa3056e1aeb44496e124a3bf7f2876dbc1bb60f9
qinyuanpei <qinyuanpei@163.com>
2024-12-21 23:02:45 +0800

    更新项目

--- hushuo/src/components/Navbar.jsx
+++ hushuo/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   const location = useLocation()
 
   return (
-    <nav className="bg-white shadow-sm">
+    <nav className="bg-white shadow-sm fixed top-0 left-0 right-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
