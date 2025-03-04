8d4f58095fa75c050af8c61001c3a8590c4cea2c
qinyuanpei <qinyuanpei@163.com>
2025-03-04 13:38:41 +0800

    完成从 Python.NET 到 CSnake 的迁移

---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Assets/Scripts/reranker.py
@@ -0,0 +1,15 @@
+﻿import modelscope
+from FlagEmbedding import FlagAutoReranker
+import os
+
+model_name = os.environ.get('RERANKER_MODEL_NAME', default='BAAI/bge-reranker-v2-m3')
+model_dir = modelscope.snapshot_download(model_name, revision='master')
+reranker = FlagAutoReranker.from_finetuned(model_dir, use_fp16=True)
+
+def compute_score(pairs: list[str]) -> float:
+	scores = reranker.compute_score(pairs, normalize=True)
+	return scores[0]
+
+def compute_scores(pairs: list[list[str]]) -> list[float]:
+	return reranker.compute_score(pairs, normalize=True)
+
