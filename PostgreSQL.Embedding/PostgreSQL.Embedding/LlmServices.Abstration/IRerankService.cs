8f876e264f5695f92eb3e92baf052d806920b934
qinyuanpei <qinyuanpei@163.com>
2025-03-04 15:17:29 +0800

    更新项目

--- PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices.Abstration/IRerankService.cs
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/LlmServices.Abstration/IRerankService.cs
@@ -4,7 +4,6 @@ namespace PostgreSQL.Embedding.LlmServices.Abstration
 {
     public interface IRerankService
     {
-        double Compute(string a, string b);
         IEnumerable<RerankResult<T>> Sort<T>(string question, List<T> documents, Expression<Func<T,string>> keySelector);
     }
 
