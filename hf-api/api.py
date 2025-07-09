402521445314d8db76467e302305d6184b4b0b50
qinyuanpei <qinyuanpei@163.com>
2025-07-09 15:15:40 +0800

    引入 uv，支持 image_to_text

--- hf-api/api.py
+++ hf-api/api.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Request, Path
 from models import CompletionRequest, ChatCompletionRequest, EmbeddingsRequest, EmbeddingsObjectResponse, EmbeddingsResponse, Usage, CompletionResponse, CompletionResponseChoice, ChatCompletionResponse, ChatCompletionResponseChoice, ChatMessage
 from fastapi import FastAPI, HTTPException
 from completion import get_chat_completion_async, get_text_completion_async
 from embeddings import get_embeddings_async
-import os, uvicorn, asyncio
+import os, uvicorn, asyncio, io
 from uvicorn.config import LOGGING_CONFIG
+from PIL import Image
+from image_to_text import convert_image_to_text, translate
 
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
 app = FastAPI(title='A OpenAI Compatible API for HuggingFace')
@@ -40,6 +43,28 @@ async def completions(request: CompletionRequest):
         choices=[CompletionResponseChoice(text=text, index=0, finish_reason='length', logprobs=None)],
         usage=usage
     )
+
+@app.post("/models/{model_name:path}", openapi_extra={
+    "requestBody": {
+        "content": {
+            "image/png": {"schema": {"type": "string", "format": "binary"}},
+            "image/jpeg": {"schema": {"type": "string", "format": "binary"}},
+        }
+    }
+})
+async def image_to_text(
+    request: Request, 
+    model_name: Annotated[str, Path(title="The model name from Hugging Face, e.g. Salesforce/blip-image-captioning-base")],
+):
+    request_body: bytes = await request.body()
+    image = Image.open(io.BytesIO(request_body)).convert('RGB')
+    generated_text = convert_image_to_text(image, model_name=model_name)
+    #generated_text = translate('Chinese', generated_text, model_name="utrobinmv/t5_translate_en_ru_zh_large_1024") 
+    return [{"generated_text": generated_text}]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
     
 if __name__ == '__main__':
     LOGGING_CONFIG["formatters"]["access"]["fmt"] = ("%(asctime)s " + LOGGING_CONFIG["formatters"]["access"]["fmt"])
