0642a52931fead09b68a4bc483a72a0da921e06f
qinyuanpei <qinyuanpei@163.com>
2026-03-24 13:50:30 +0800

    更新博客

---
+++ PostgreSQL.Embedding/PostgreSQL.Embedding/Domain/Models/Search/ImageSearchResult.cs
@@ -0,0 +1,15 @@
+﻿namespace PostgreSQL.Embedding.Domain.Models.Search
+{
+    public class ImageSearchResult
+    {
+        public string Keyword { get; set; }
+        public List<ImageEntry> Entries { get; set; } = new List<ImageEntry>();
+    }
+
+    public class ImageEntry
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
